2178de02bee4c0979cad23c1662d5045ae4b65f6
Даша Данильченко <https://github.com/DashaDanilchenko/homework>
2022-08-09 13:20:09 +0300

    task 1-7

--- script.js
+++ script.js
@@ -55,3 +55,18 @@ function Dog() {
 // Use the Dog constructor from the last lesson to create a new instance of Dog, assigning it to a variable hound.
 
 let hound  = new Dog()
+
+
+// 7
+// Create another Dog constructor. This time, set it up to take the parameters name and color, and have the property numLegs fixed at 4.
+// Then create a new Dog saved in a variable terrier. Pass it two strings as arguments for the name and color properties.
+
+function DogA(name, color) {
+    this.name = name;
+    this.color = color;
+    this.numLegs = 4;
+  }
+  
+  let terrier = new DogA();
+  terrier.name = "Carlos";
+  terrier.color = "white"
